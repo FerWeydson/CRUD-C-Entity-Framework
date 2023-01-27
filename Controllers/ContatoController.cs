cc64de46d7c21548f9fe92ccd6a9bfc586e7667b
fer.weydson <fer.weydson@gmail.com>
2023-01-27 12:49:00 -0300

    CRUD finalizado

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -18,11 +18,49 @@ namespace teste_api.Controllers
             _context = context;
         }
         [HttpPost]
-        public ActionResult Create(Contato contato)
+        public IActionResult Create(Contato contato)
         {
             _context.Add(contato);
             _context.SaveChanges();
             return Ok(contato   );
+        }
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if(contato == null)
+                return NotFound();
+
+            return Ok(contato);
+            
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarContato(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+            if (contatoBanco == null)
+                return NotFound();
+            
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeletarContato(int id){
+            var contatoBanco = _context.Contatos.Find(id);
+            if (contatoBanco == null)
+                return NotFound();
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+            return NoContent();
+
         }
     }
 }
